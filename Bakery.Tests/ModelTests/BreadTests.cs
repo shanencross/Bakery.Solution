df68857897ff14bca13c8673473a5bf45de9332b
Shanen Cross <shanencross@yahoo.com>
2021-04-25 00:46:21 -0700

    Add test for Count and several tests for TotalPrice

--- Bakery.Tests/ModelTests/BreadTests.cs
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -20,5 +20,104 @@ namespace Bakery.Tests
       int result = Bread.PricePerItem;
       Assert.AreEqual(expectedResult, result);
     }
+
+    [TestMethod]
+    public void Count_CountIsInitializedCorrectly_23()
+    {
+      int expectedResult = 23;
+      Bread bread = new Bread(expectedResult);
+      int result = bread.Count;
+      Assert.AreEqual(expectedResult, result);
+    }
+
+    [TestMethod]
+    public void TotalPrice_GetRightPriceForOneBread_5()
+    {
+      int expectedResult = 5;
+      int count = 1;
+      Bread bread = new Bread(count);
+      int result = bread.TotalPrice;
+      Assert.AreEqual(expectedResult, result);
+    }
+
+    [TestMethod]
+    public void TotalPrice_GetRightPriceForTwoBreads_10()
+    {
+      int expectedResult = 10;
+      int count = 2;
+      Bread bread = new Bread(count);
+      int result = bread.TotalPrice;
+      Assert.AreEqual(expectedResult, result);
+    }
+
+    
+    [TestMethod]
+    public void TotalPrice_BuyTwoGetOneFree_10()
+    {
+      int expectedResult = 10;
+      int count = 3;
+      Bread bread = new Bread(count);
+      int result = bread.TotalPrice;
+      Assert.AreEqual(expectedResult, result);
+    }
+    
+    [TestMethod]
+    public void TotalPrice_BuyFourBreadsWithDeal_15()
+    {
+      int expectedResult = 15;
+      int count = 4;
+      Bread bread = new Bread(count);
+      int result = bread.TotalPrice;
+      Assert.AreEqual(expectedResult, result);
+    }
+    
+    [TestMethod]
+    public void TotalPrice_BuyFiveBreadsWithDeal_20()
+    {
+      int expectedResult = 20;
+      int count = 5;
+      Bread bread = new Bread(count);
+      int result = bread.TotalPrice;
+      Assert.AreEqual(expectedResult, result);
+    }
+    
+    [TestMethod]
+    public void TotalPrice_GetTwoDealsByBuyingSixBreads_20()
+    {
+      int expectedResult = 20;
+      int count = 6;
+      Bread bread = new Bread(count);
+      int result = bread.TotalPrice;
+      Assert.AreEqual(expectedResult, result);
+    }
+    
+    [TestMethod]
+    public void TotalPrice_BuySevenBreadsWithDeal_25()
+    {
+      int expectedResult = 25;
+      int count = 7;
+      Bread bread = new Bread(count);
+      int result = bread.TotalPrice;
+      Assert.AreEqual(expectedResult, result);
+    }
+    
+    [TestMethod]
+    public void TotalPrice_GetThreeDeals_30()
+    {
+      int expectedResult = 30;
+      int count = 9;
+      Bread bread = new Bread(count);
+      int result = bread.TotalPrice;
+      Assert.AreEqual(expectedResult, result);
+    }
+
+    public void TotalPrice_GetTwentyNineBreadsWithDeals_100()
+    {
+      int expectedResult = 100;
+      int count = 29;
+      Bread bread = new Bread(count);
+      int result = bread.TotalPrice;
+      Assert.AreEqual(expectedResult, result);
+    }
   }
 }
