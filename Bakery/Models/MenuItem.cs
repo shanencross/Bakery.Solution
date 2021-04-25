77c49ba76885fdd005b544bd0e4e84a937744075
Shanen Cross <shanencross@yahoo.com>
2021-04-25 04:39:42 -0700

    Remove protected setters, not needed now

--- Bakery/Models/MenuItem.cs
+++ Bakery/Models/MenuItem.cs
@@ -2,8 +2,8 @@ namespace Bakery.Models
 {
   public abstract class MenuItem
   {
-    public int Count { get; protected set; }
-    public int TotalPrice { get; protected set; }
+    public int Count { get; }
+    public int TotalPrice { get; }
 
     public MenuItem(int count)
     {
