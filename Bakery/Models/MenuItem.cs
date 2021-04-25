a4d475d7ea64974936465473ddb8cfb73fa02879
Shanen Cross <shanencross@yahoo.com>
2021-04-25 04:34:22 -0700

    Fill out MenuItem superclass

--- Bakery/Models/MenuItem.cs
+++ Bakery/Models/MenuItem.cs
@@ -2,6 +2,15 @@ namespace Bakery.Models
 {
   public abstract class MenuItem
   {
+    public int Count { get; protected set; }
+    public int TotalPrice { get; protected set; }
 
+    public MenuItem(int count)
+    {
+      Count = count;
+      TotalPrice = CalculatePrice();
+    }
+
+    protected abstract int CalculatePrice();
   }
 }
