ca2c7fa75c96c3612d7588ba42529c0c6f61d34f
Shanen Cross <shanencross@yahoo.com>
2021-04-25 00:32:07 -0700

    Add and fail test for price per item static value; initialized with wrong value

--- Bakery.Tests/ModelTests/PastryTests.cs
+++ Bakery.Tests/ModelTests/PastryTests.cs
@@ -12,5 +12,13 @@ namespace Bakery.Tests
       Pastry pastry = new Pastry();
       Assert.AreEqual(typeof(Pastry), pastry.GetType());
     }
+
+    [TestMethod]
+     public void PricePerItem_GetsCorrectValue_2()
+    {
+      int expectedResult = 2;
+      int result = Pastry.PricePerItem;
+      Assert.AreEqual(expectedResult, result);
+    }
   }
 }
