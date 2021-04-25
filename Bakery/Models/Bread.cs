63e9818801a75591b083d528f069e18a52723c70
Shanen Cross <shanencross@yahoo.com>
2021-04-25 03:43:38 -0700

    Fix braces

--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -1,5 +1,7 @@
-namespace Bakery.Models {
-  public class Bread {
+namespace Bakery.Models 
+{
+  public class Bread 
+  {
     public static int PricePerItem { get; } = 5;
     public int Count { get; }
     public int TotalPrice { get; }
