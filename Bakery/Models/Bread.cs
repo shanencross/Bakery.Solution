66200d1be9fadb3e1d895557a757db15cc3b1fd3
Shanen Cross <shanencross@yahoo.com>
2021-04-25 00:24:54 -0700

    Add properties to Bread and Pastry classes

--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -1,5 +1,8 @@
 namespace Bakery.Models {
   public class Bread {
+    public static int PricePerItem { get; } = 5;
+    public int Count { get; }
+    public int TotalPrice { get; }
 
   }
 }
--- Bakery/Models/Pastry.cs
+++ Bakery/Models/Pastry.cs
@@ -2,6 +2,9 @@ namespace Bakery.Models
 {
   public class Pastry 
   {
+    public static int PricePerItem { get; } = 5;
+    public int Count { get; }
+    public int TotalPrice { get; }
 
   }
 }
